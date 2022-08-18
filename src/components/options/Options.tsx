91f5d88db80915f449797d0fb10ef533d684be30
Mahendra Chauhan <mikesch803@gmail.com>
2022-08-18 22:52:48 +0530

    update icon-data

--- src/components/options/Options.tsx
+++ src/components/options/Options.tsx
@@ -1,26 +1,31 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/solid'
-import { NavLink } from 'react-router-dom'
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { NavLink } from "react-router-dom";
 
-function classNames(...classes:any) {
-  return classes.filter(Boolean).join(' ')
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
 }
 
 export function Options() {
-    const [isOpen, setIsOpen] = useState(false)
   return (
     <Menu as="div" className="relative sm:inline-block text-left hidden">
-      <div onClick={()=>setIsOpen(!isOpen)}>
+      <div>
         <Menu.Button className="inline-flex justify-center w-full border-gray-300 px-4 py-2 bg-white text-sm font-medium text-gray-700 ">
-          {/* <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" /> */}
-         {!isOpen ? <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-</svg> :
-<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-</svg> }
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
         </Menu.Button>
       </div>
 
@@ -36,12 +41,12 @@ export function Options() {
         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
-              {({ active }:any) => (
+              {({ active }: any) => (
                 <NavLink
                   to="/projects"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-md font-semibold'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-md font-semibold"
                   )}
                 >
                   Projects
@@ -49,12 +54,12 @@ export function Options() {
               )}
             </Menu.Item>
             <Menu.Item>
-              {({ active }:any) => (
+              {({ active }: any) => (
                 <NavLink
                   to="/blogs"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-md font-semibold'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-md font-semibold"
                   )}
                 >
                   Blogs
@@ -62,22 +67,21 @@ export function Options() {
               )}
             </Menu.Item>
             <Menu.Item>
-              {({ active }:any) => (
+              {({ active }: any) => (
                 <a
-                href="#connect"
+                  href="#connect"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-md font-semibold'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-md font-semibold"
                   )}
                 >
                   Connect
                 </a>
               )}
             </Menu.Item>
-            
           </div>
         </Menu.Items>
       </Transition>
     </Menu>
-  )
+  );
 }
